3844daebce35d676de7fcb8a014ef63a25e24a2e
Marcus Fábio Salviano <marcussalviano@gmail.com>
2024-07-01 01:58:31 -0400

    Organizando imports

--- src/main/java/br/com/preco_justo/domain/Venda/VendaRepository.java
+++ src/main/java/br/com/preco_justo/domain/Venda/VendaRepository.java
@@ -1,12 +1,6 @@
 package br.com.preco_justo.domain.Venda;
 
-import br.com.preco_justo.domain.Cliente.Cliente;
-import br.com.preco_justo.domain.Pato.Pato;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
 }
